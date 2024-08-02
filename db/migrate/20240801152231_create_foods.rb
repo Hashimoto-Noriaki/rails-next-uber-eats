afd64a41ff8c9ef6c8e94eaaefca6beb3b936ab3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-02 10:36:59 +0900

    空白の修正

--- db/migrate/20240801152231_create_foods.rb
+++ db/migrate/20240801152231_create_foods.rb
@@ -5,6 +5,7 @@ class CreateFoods < ActiveRecord::Migration[7.0]
       t.string :name, null: false
       t.integer :price, null: false
       t.text :description, null: false
+      
       t.timestamps
     end
   end
