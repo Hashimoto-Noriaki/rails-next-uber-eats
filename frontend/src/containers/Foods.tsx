60fdb5be88312c3762c7ea84b01eca8f8b4ace54
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-04 02:02:05 +0900

    Foodsコンポーネントを修正

--- frontend/src/containers/Foods.tsx
+++ frontend/src/containers/Foods.tsx
@@ -1,9 +1,15 @@
 import React, { Fragment } from 'react';
+import { useRouter} from 'next/router';
 
 export const Foods: React.FC = () => {
-  return (
-    <Fragment>
-      フード一覧
-    </Fragment>
-  );
+    const router = useRouter();
+    const { restaurantsId } = router.query;
+    return (
+        <>
+            フード一覧
+            <p>
+                restaurantsIdは {restaurantsId} です
+            </p>
+        </>
+    );
 };
