f571b6c46e81889906e47e3065444a046f3774c3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-04 02:53:23 +0900

    APIリクエストに関する定数を定義

---
+++ frontend/src/constants.ts
@@ -0,0 +1,9 @@
+export const REQUEST_STATE = {
+    INITIAL: 'INITIAL' as 'INITIAL',
+    LOADING: 'LOADING' as 'LOADING',
+    OK: 'OK' as 'OK',
+} as const;
+
+export const HTTP_STATUS_CODE = {
+    NOT_ACCEPTABLE: 406,
+} as const;
