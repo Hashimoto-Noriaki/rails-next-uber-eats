953fb268f85a962e1334c24501ab101bcfc2415b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-05 00:39:37 +0900

    Fragment追記

--- frontend/src/containers/Restaurants.tsx
+++ frontend/src/containers/Restaurants.tsx
@@ -2,8 +2,8 @@ import React, {Fragment} from 'react';
 
 export const Restaurant: React.FC = () => {
     return (
-        <>
+        <Fragment>
           レストラン一覧
-        </>
+        </Fragment>
       );
 }
