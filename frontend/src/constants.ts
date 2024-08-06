3a337d0760eff0293b24fdc2dee2628e3f325541
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-07 01:13:46 +0900

    レストラン一覧ページを完成

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -2,6 +2,7 @@ export const REQUEST_STATE = {
     INITIAL: 'INITIAL' as 'INITIAL',
     LOADING: 'LOADING' as 'LOADING',
     OK: 'OK' as 'OK',
+    ERROR: 'ERROR',
 } as const;
 
 export const HTTP_STATUS_CODE = {
